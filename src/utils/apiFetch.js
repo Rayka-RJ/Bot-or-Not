c26cdf09439ba393ff5d4af2ac354828cac1d86f
Rayka-RJ <zhangrj15@gmail.com>
2025-06-11 20:55:08 +0800

    test connection

--- src/utils/apiFetch.js
+++ src/utils/apiFetch.js
@@ -1,7 +1,10 @@
 import config from '../config';
 
 export async function apiFetch(endpoint, options = {}) {
-    const url = endpoint.startsWith('http') ? endpoint : `${config.apiUrl}${endpoint}`;
+    const path = endpoint.startsWith('/') ? endpoint : `/${endpoint}`;
+    //complete url or base URL
+    const url = endpoint.startsWith('http') ? endpoint : `${config.apiUrl}${path}`;
+    
     const headers = {
       ...options.headers,
       "x-ai-mode": localStorage.getItem("aiMode") || "free",
